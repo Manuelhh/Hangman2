23612e7f635202348a25cfcd1af3565f74abec0f
Manuel Hinojosa <mangud.hinojosa@gmail.com>
2023-02-23 11:21:58 -0500

    worked on hangman page component: added containers for element components and styled them

--- src/PageComponents/HangmanPage/HangmanPage.tsx
+++ src/PageComponents/HangmanPage/HangmanPage.tsx
@@ -1,5 +1,18 @@
 import "./HangmanPage.css";
 
 export const HangmanPage: React.FC = () => {
-  return <div className="hangmanPage">Hangman page</div>;
+  return (
+    <div className="hangmanPage">
+      <div className="mainConatiner">
+        <div className="outcomeMessageContainer"></div>
+        <div className="leftContainer">
+          <div className="animationContainer"></div>
+          <div className="wordToGuessContainer"></div>
+        </div>
+        <div className="writeContainer">
+          <div className="keyboardContainer"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
